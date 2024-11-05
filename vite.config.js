29d9f69f3cfa63897f9fcf23e1f541f7fc38b01a
opeyemi <opeyemi@gmail.com>
2024-11-05 18:07:53 +0100

    woman

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
+  server: mode === 'development' ? {
     proxy: {
       '/api': {
-        target: 'http://localhost:3003',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
     },
+  } : {},
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/react-fontawesome'],
+    },
   },
-   build:{
-    rollupOptions:{
-      external:['@fortawesome/react-fontawesome'],
-    }
-   }
-});
+  base: './' 
+}));
